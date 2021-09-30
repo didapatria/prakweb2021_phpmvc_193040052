7107e07fb7a03c76568cc656415c5d618e3d8e12
didapatria <didapatria3@gmail.com>
2021-09-30 09:40:00 +0700

    Memulai video Membuat Aplikasi MVC dengan PHP #5 View

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -3,9 +3,9 @@
 class About
 {
   // method
-  public function index($nama = 'Adinda', $pekerjaan = 'Mahasiswa')
+  public function index($nama = 'Adinda', $pekerjaan = 'Mahasiswa', $umur = 22)
   {
-    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+    echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan. Saya berumur $umur tahun.";
   }
   public function page()
   {
