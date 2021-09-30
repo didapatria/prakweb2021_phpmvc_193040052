971f65a4e77c25c94d4c7ce1a95281e8a02e7db3
didapatria <didapatria3@gmail.com>
2021-09-30 08:01:01 +0700

    Menyelesaikan video Membuat Aplikasi MVC dengan PHP #3 Routing

--- app/core/App.php
+++ app/core/App.php
@@ -5,6 +5,18 @@ class App
   // constructor
   public function __construct()
   {
-    var_dump($_GET);
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  // method
+  public function parseURL()
+  {
+    if (isset($_GET['url'])) {
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
   }
 }
